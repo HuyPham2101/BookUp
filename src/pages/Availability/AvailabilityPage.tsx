4b7e6b3ecd5340673ae39c3f54adcdda678de3d9
stdabahc <david.bahcecioglu@stud.h-da.de>
2021-02-13 16:42:20 +0100

    #7 feat: added available Page

--- src/pages/Availability/AvailabilityPage.tsx
+++ src/pages/Availability/AvailabilityPage.tsx
@@ -1,24 +1,55 @@
 import { TimePicker,  List ,Card} from 'antd';
-import { useState } from 'react';
-import { Day } from '../../components/Day';
+import { useContext, useEffect, useState } from 'react';
+import { authContext } from '../../contexts/AuthenticationContext';
+import { Day, DayAvailability, DayRow } from './components/DayRow';
+import jwt_decode from "jwt-decode";
 
 const { RangePicker } = TimePicker;
 
 export const AvailabilityPage = () => {
-    const [hour,setHour] = useState('')
-    const [minute, setMinute] = useState('')
+    const {token} = useContext(authContext);
+    const [daysAvailability, setDaysAvailability] = useState<DayAvailability[]>([]);
+
+
+    const fetchAvailability = async function () {
+        let decoded : any = jwt_decode(token || "");
+        const availabilityRequest = await fetch("/api/availability/" + decoded.id, {
+            method: "GET",
+            headers: {"Content-Type": "application/json"},
+        });
+        if (availabilityRequest.status === 200) {
+            const availabilityJSON = await availabilityRequest.json();
+            console.log(availabilityJSON.data);
+            setDaysAvailability(availabilityJSON.data);
+            console.log(daysAvailability);
+
+        }
+    }
+
+    useEffect(() => {
+        fetchAvailability();
+    }, []);
+    
 
     return(
         <div style = {{display :'flex', alignContent : 'center', justifyContent : "center"}} >
             <Card title="Set your weekly Hours" extra={<a href="#">More</a>} style={{ width: 500 }}>
             <List>
-            <List.Item> <Day title = "Mon"/> </List.Item>
-            <List.Item> <Day title = "Tue"/> </List.Item>
-            <List.Item> <Day title = "Wed"/> </List.Item>
-            <List.Item> <Day title = "Thu"/> </List.Item>
-            <List.Item> <Day title = "Fri"/> </List.Item>
-            <List.Item> <Day title = "Sat"/> </List.Item>
-            <List.Item> <Day title = "Sun"/> </List.Item>
+            {/* <List.Item> <DayRow title = "Mon"/> </List.Item>
+            <List.Item> <DayRow title = "Tue"/> </List.Item>
+            <List.Item> <DayRow title = "Wed"/> </List.Item>
+            <List.Item> <DayRow title = "Thu"/> </List.Item>
+            <List.Item> <DayRow title = "Fri"/> </List.Item>
+            <List.Item> <DayRow title = "Sat"/> </List.Item>
+            <List.Item> <DayRow title = "Sun"/> </List.Item> */}
+            {/* {daysAvailability.map((dayAvailability) => {
+                <List.Item> <DayRow {...dayAvailability} /> </List.Item>
+            })} */}
+
+            {daysAvailability.map((day)=> (
+                <List.Item key={day.id}> <DayRow day={day} fetchDays={() => {fetchAvailability()}}/></List.Item>
+            ))}
+            {/* <List.Item> <DayRow {daysAvailability[1]} /> </List.Item> */}
             </List>
             </Card>
         </div>
--- src/pages/Availability/components/DayRow.tsx
+++ src/pages/Availability/components/DayRow.tsx
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React, { useState } from 'react';
 import { TimePicker,Checkbox } from 'antd';
 import moment from 'moment';
 
@@ -14,22 +14,67 @@ export enum Day {
     SUNDAY = "SUNDAY"
 }
 
+export type DayItemProps = {
+    day: DayAvailability;
+    fetchDays: () => void;
+}
+
 export type DayAvailability = {
      day: Day,
      fromTimeHour: number,
      fromTimeMinute: number,
      endTimeHour: number,
      endTimeMinute: number,
+     id: number,
+     createdAt: Date,
+     updatedAt: Date,
+     deletedAt: Date,
 }
 
-export const DayRow = ( dayAvailability: DayAvailability ) => {
+export const DayRow: React.FC<DayItemProps> = ({
+    day, fetchDays = () => {
+
+    }
+
+}) => {
+ 
+    const [checkedBox, setCheckedBox] = useState(true);
+
     return (
     <div>
         <form action="" style = {{display:'flex' , justifyContent: 'space-evenly'}}>
-            <Checkbox/>
-            {dayAvailability.day}
-            <RangePicker defaultValue={[moment(dayAvailability.fromTimeHour), moment(dayAvailability.endTimeHour)]} showSecond = {false} showNow= {false} style = {{}}/>
+            <Checkbox onChange= {(e) => setCheckedBox(e.target.checked)} defaultChecked={true}/>
+            <div style= {{position:"absolute", top: '50%'}}>
+            {day.day}
+            </div>
+            {checkedBox && 
+            <RangePicker defaultValue={[moment(day.fromTimeHour), moment(day.endTimeHour)]} showSecond = {false} showNow= {false} style = {{}}/>            
+            }
+            
+            {!checkedBox && (
+                <p>Unavailable!</p>
+            )}
+            
         </form>
     </div>
     )
+
 }
+
+// export const DayRow = ( dayAvailability: DayAvailability ) => {
+//     const [checkedBox, setCheckedBox] = useState(true);
+
+//     return (
+//     <div>
+//         <form action="" style = {{display:'flex' , justifyContent: 'space-evenly'}}>
+//             <Checkbox onChange= {(e) => setCheckedBox(e.target.checked)}/>
+//             {dayAvailability.day}
+//             <RangePicker defaultValue={[moment(dayAvailability.fromTimeHour), moment(dayAvailability.endTimeHour)]} showSecond = {false} showNow= {false} style = {{}}/>
+            
+//             {!checkedBox && (
+//                 <p>Unvailable!</p>
+//             )}
+//         </form>
+//     </div>
+//     )
+// }
