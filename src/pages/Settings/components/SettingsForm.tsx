f1258bfae6e536320ac07d642deee09235797c1a
Abdurrahman Yigen <stabyige@stud.h-da.de>
2021-02-16 21:39:30 +0100

    #13 feat: implement to write password two times to change

--- src/pages/Settings/components/SettingsForm.tsx
+++ src/pages/Settings/components/SettingsForm.tsx
@@ -1,18 +1,33 @@
 import { Button, Form, Input, message, Space } from "antd"
+import { ChangeEvent, useState } from "react"
 import { User } from "../../../components/EntityTypes"
 
+interface UserData {
+    username?: string;
+    password?: string;
+    repeatedPassword?: string;
+}
+
 export const SettingsForm: React.FC<{ fetchUser: () => void, user: User | undefined }> = ({ user, fetchUser }) => {
+    const [changedUserData, setChangedUserData] = useState<UserData>({
+        username: user?.userName
+    });
 
     const saveChanges = async () => {
         const userName = (document.getElementById("userNameInput") as HTMLInputElement).value;
-        const password = (document.getElementById("passwordInput") as HTMLInputElement).value;
+        const InputPassword = (document.getElementById("passwordInput") as HTMLInputElement);
+        const InputRepeatedPassword = (document.getElementById("repeatPassword") as HTMLInputElement);
         let savingUser;
-        if (!password) {
+        if (!InputPassword.value) {
             savingUser = { userName: userName }
         } else {
+            if (InputPassword.value !== InputRepeatedPassword.value) {
+                message.error("Passwords not matching!")
+                return;
+            }
             savingUser = {
                 userName: userName,
-                password: password
+                password: InputPassword.value
             }
         }
         await fetch(`/api/user/${user?.id}`, {
@@ -21,8 +36,27 @@ export const SettingsForm: React.FC<{ fetchUser: () => void, user: User | undefi
             body: JSON.stringify(savingUser),
         })
         message.success("Changes saved!")
-        document.getElementById("saveBtn")?.setAttribute("disabled", "disabled")
+        setChangedUserData({ ...changedUserData, password: "", repeatedPassword: "" });
+        disableSaveButton();
         fetchUser();
+    }
+
+    const fieldDidChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setChangedUserData({ ...changedUserData, [e.target.name]: e.target.value })
+        const userName = (document.getElementById("userNameInput") as HTMLInputElement).value;
+        const InputPassword = (document.getElementById("passwordInput") as HTMLInputElement);
+        const InputRepeatedPassword = (document.getElementById("repeatPassword") as HTMLInputElement);
+        if (userName === user?.userName &&
+            !InputPassword.value &&
+            !InputRepeatedPassword.value) {
+            disableSaveButton();
+        } else {
+            enableSaveButton();
+        }
+    }
+
+    const disableSaveButton = () => {
+        document.getElementById("saveBtn")?.setAttribute("disabled", "disabled");
     }
 
     const enableSaveButton = () => {
@@ -36,18 +70,18 @@ export const SettingsForm: React.FC<{ fetchUser: () => void, user: User | undefi
                 wrapperCol={{ span: 6 }}
                 labelAlign="left"
                 onFinish={saveChanges}
-                onChange={enableSaveButton}
                 style={{ margin: 10 }}
             >
                 <Form.Item label="Username">
-                    <Input id="userNameInput" defaultValue={user.userName} />
+                    <Input id="userNameInput" name="username" value={changedUserData?.username} onChange={fieldDidChange} defaultValue={user.userName} minLength={2} />
                 </Form.Item>
                 <Form.Item label="E-Mail">
                     <Input defaultValue={user.email} disabled />
                 </Form.Item>
                 <Form.Item label="Change password">
                     <Space direction="vertical">
-                        <Input.Password id="passwordInput" placeholder="input password" />
+                        <Input.Password id="passwordInput" name="password" value={changedUserData?.password} onChange={fieldDidChange} placeholder="new password" />
+                        <Input.Password id="repeatPassword" name="repeatesPassword" value={changedUserData?.repeatedPassword} onChange={fieldDidChange} placeholder="repeat password" />
                     </Space>
                 </Form.Item>
                 <Form.Item>
