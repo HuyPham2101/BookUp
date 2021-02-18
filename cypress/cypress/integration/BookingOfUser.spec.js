1d1b2457af544036b657ed704da721766ffcfd95
Pham Duc Le Huy <pham.huy@stud.h-da.de>
2021-02-18 14:21:40 +0100

    #18 test(add Availability, Login, Register Pages test)

--- cypress/cypress/integration/BookingOfUser.spec.js
+++ cypress/cypress/integration/BookingOfUser.spec.js
@@ -2,6 +2,9 @@ describe("BookingsOfUser" , () => {
     it("can add Upcoming events ", ()=> {
         cy.loginNewUser({}).then((user) => {
             cy.visit("/meetings");
+            cy.findByText(/Upcoming/i).should('exist');
+            cy.findByText(/Past/i).should('exist');
+            cy.findByText(/All/i).should('exist');
             cy.screenshot();
         })
     })
