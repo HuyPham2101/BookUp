1d4d823380ee260a3c7d6f56e48a8c788a5ea748
Pham Duc Le Huy <pham.huy@stud.h-da.de>
2021-02-19 00:03:01 +0100

    test(added more test to dashboard and Availability)

--- cypress/cypress/integration/dashboard.spec.js
+++ cypress/cypress/integration/dashboard.spec.js
@@ -9,6 +9,26 @@ describe("Dashboard" , () => {
             cy.get('[data-cy=Select-Minute-Testing]').clear().type(15)
             cy.findByText("Submit").click();
             cy.findByText("Copy Link").should('exist')
+            cy.findByText("testingTitle").should('exist')
+            cy.findByText("TestingDescription").should('exist')
+            cy.screenshot();
+        })
+    })
+    it("can redirect to Availability Page", ()=> {
+        cy.loginNewUser({}).then((user) => {
+            cy.visit("/dashboard");
+            cy.screenshot();
+            cy.findByText(/Availability/i).click();
+            cy.url().should("contain", "/Availability");
+            cy.screenshot();
+        })
+    })
+    it("can redirect to Meeting Page" , ()=> {
+        cy.loginNewUser({}).then((user) => {
+            cy.visit("/dashboard");
+            cy.screenshot();
+            cy.findByText(/Meetings/i).click();
+            cy.url().should("contain", "/meetings");
             cy.screenshot();
         })
     })
